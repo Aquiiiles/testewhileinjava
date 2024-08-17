5017a5a1726bf9239183fc2fd3a267ba3f2737d4
Aquiles Duarte <aquilesaluno@outlook.com>
2024-08-17 17:41:02 -0300

    first commit

---
+++ teste.java
@@ -0,0 +1,13 @@
+public class teste {
+
+    public static void main(String[] args) {
+        System.out.println("Seja bem-vindo ao meu teste de while");
+
+        int contador = 10;
+
+        while (contador <= 15) {
+            System.out.println(contador);
+            contador++;
+        }
+    }
+}
